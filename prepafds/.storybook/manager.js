cb2a92f93e21f80a44081c12a68c7c987abd912d
Thiago Freitas <thiago.gn7@gmail.com>
2023-11-06 03:52:24 -0300

    Add new theming to storybook

--- prepafds/.storybook/manager.js
+++ prepafds/.storybook/manager.js
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/addons'
 import { themes } from '@storybook/theming'
+import prepafTheme from './prepafTheme'
 
 addons.setConfig({
-    theme:themes.light,
+    theme:prepafTheme,
 })
---
+++ prepafds/.storybook/prepafTheme.js
@@ -0,0 +1,11 @@
+// .storybook/prepaftheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Prepaf - Design System',
+  brandUrl: 'https://prepaf.projetosuff.com.br/',
+  brandImage: 'https://prepaf.projetosuff.com.br/wp-content/uploads/2021/12/cropped-logo-250x60v3.png',
+  brandTarget: '_self',
+});
