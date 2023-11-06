d1b7580ba5bbbf6bb39494da94033af7f8405b53
Thiago Freitas <43794957+thiagofsf@users.noreply.github.com>
2023-11-06 15:46:26 -0300

    Footer component add

---
+++ prepafds/src/components/Footer.stories.tsx
@@ -0,0 +1,77 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Footer, Props } from './Footer'
+import { Text} from './Text'
+import { Heading } from './Heading'
+
+export default {
+    title: 'Components/Footer',
+    component: Footer,
+    tags: ['autodocs'],
+    args: {
+        children:'Lorem ipsum.',
+        align:'right'
+    },
+    argTypes:{
+        align: {
+            options: ['left', 'center', 'right', 'justify'],
+            control:{
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<Props>
+
+export const Default: StoryObj<Props> = {}
+
+export const AlignLeft: StoryObj<Props> = {
+    args:{
+        align:'left'
+    }
+}
+
+export const AlignCenter: StoryObj<Props> = {
+    args:{
+        align:'center'
+    }
+}
+
+export const AlignRight: StoryObj<Props> = {
+    args:{
+        align:'right'
+    }
+}
+
+export const AlignJustify: StoryObj<Props> = {
+    args:{
+        align:'justify'
+    }
+}
+
+export const CustomComponent: StoryObj<Props> = {
+    args:{
+        asChild: true,
+        align: 'center',
+        children:(
+            <>
+            <div className='w-full text-center'>
+            <Heading size='sm'>Exemplo de Rodapé com Componentes</Heading>
+            </div>
+            <div className='w-full text-center'>
+            <Text>Você pode passar um ou mais componentes usando a propriedade 'AsChild' para o rodapé</Text>
+            </div>
+            </>
+        ),
+    },
+    argTypes: {
+        children:{
+            table:{
+                disable:true,
+            }
+        },
+        asChild: {
+            table:{
+                disable:true,
+            }
+        }
+    }
+}
---
+++ prepafds/src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react';
+import { clsx } from "clsx";
+import {Text} from './Text';
+import { Slot } from "@radix-ui/react-slot";
+
+export interface Props {
+    align: 'right' | 'left' | 'center' | 'justify';
+    children: ReactNode;
+    asChild?: boolean;
+}
+
+export function Footer({children, align = 'left', asChild}: Props) {
+
+    return (
+        <div className={clsx(
+            'w-full bg-gray-200 p-5 flex',
+            {
+                'justify-center': align == 'center',
+                'justify-start': align == 'left',
+                'justify-end': align == 'right',
+            }
+        )}>
+            <div className={clsx(
+                {   
+                    'text-center': align == 'center',
+                    'text-right': align == 'right',
+                    'text-left': align == 'left',
+                    'text-justify': align == 'justify',
+                }
+            )}>
+                {asChild ? (<Slot>{children}</Slot>) : (<Text size='sm' align={align}>{children}</Text>) }
+                
+            </div>
+        </div>
+    );
+}
