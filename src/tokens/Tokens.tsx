f9abe5ed1eed32829c2c340b8ce8635b4f005899
thiago.gn7@gmail.com <43794957+thiagofsf@users.noreply.github.com>
2023-11-24 20:55:55 -0300

    globaltokens

---
+++ src/tokens/Tokens.tsx
@@ -0,0 +1,44 @@
+export const tokens = {
+    colors:{
+      'prepaf-orange':{token:'prepaf-orange', value:'#F79F4F'},
+      'prepaf-yellow':{token:'prepaf-yellow', value:'#FFDE33'},
+      //Basic Colors
+      'white':{token:'white', value: '#ffffff'},
+      'black':{token:'black', value: '#000000'},
+      'transparent':{token:'transparent', value:'transparent'},
+      //Black Variants
+      'black-800':{token:'black-800', value:'#1E1E1E'},
+      'black-1000':{token:'black-1000', value:'#000000'},
+      //Gray Variants
+      'gray-50':{token:'gray-50', value:'#F8F8F8'},
+      'gray-100':{token:'gray-100', value:'#F0F0F0'},
+      'gray-200':{token:'gray-200', value:'#EBEBEB'},
+      'gray-300':{token:'gray-300', value:'#D6D6D6'},
+      'gray-500':{token:'gray-500', value:'#B9B9B9'},
+      'gray-600':{token:'gray-600', value:'#848484'},
+      //Blue Variants
+      'blue-500':{token:'blue-500', value:'#22B4D8'},
+      //Yellow Variants
+      'yellow-200':{token:'yellow-200', value:'#F7EBAD'},
+      yellow500:{token:'yellow-500', value:'#E0BB00'},
+      //Orange Variants
+      'orange-200':{token:'orange-200', value:'#FEB470'},
+      //Green Variants
+      'green-300':{token:'green-300', value:'#A5DF28'},
+      'green-400':{token:'green-400', value:'#9ED130'},
+      //Reds Variants
+      'red-600':{token:'red-600', value:'#E93D32'},
+      'red-800':{token:'red-800', value:'#CE2D23'},
+      //Status Secondary Tokens
+      'treino':{token:'treino', value: yellow500}
+        'treino':'#E0BB00',
+      'jogo':'#B5EB1C',
+      'recuperacao':'#09BFD8',
+      'avaliacao':'#EE923D',
+    ],
+      //Status Category Colors
+      
+    },
+    ],
+    'fontsize': [],
+}
